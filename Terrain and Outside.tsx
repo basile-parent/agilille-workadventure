b0bd8f9cf27a22fdda893e8aa37f2c2349a9d6ac
Basile Parent <basile.parent.partner@decathlon.com>
2021-05-28 12:44:47 +0200

    Map designed by WA

---
+++ Terrain and Outside.tsx
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Terrain and Outside" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="Terrain and Outside.png" width="1024" height="1024"/>
+ <wangsets>
+  <wangset name="herbe" type="mixed" tile="289">
+   <wangcolor name="" color="#ff0000" tile="226" probability="1"/>
+   <wangtile tileid="193" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="194" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="225" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="226" wangid="0,1,1,1,1,1,0,0"/>
+   <wangtile tileid="256" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="257" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="258" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="288" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="289" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="290" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="320" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="321" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="322" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="eau-herbe" type="mixed" tile="689">
+   <wangcolor name="" color="#ff0000" tile="624" probability="1"/>
+   <wangtile tileid="592" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="593" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="624" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="625" wangid="0,1,1,1,1,1,0,0"/>
+   <wangtile tileid="655" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="656" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="657" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="687" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="688" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="689" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="719" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="720" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="721" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
